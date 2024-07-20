c1aff1a0aef565bb201c8104dff6902e86d75a09
Emmanuel Kipkoech <kipkoechemmanuel234@gmail.com>
2024-07-20 20:21:23 +0300

    Add card layout with images, YouTube icons, and 'Learn More' text using HTML and CSS

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementsByClassName("fab fa-youtube")[0].addEventListener("click",function(){
+    window.open("https://youtu.be/srJcIYSr34c?si=qYRysZEpf8VBpKHV")
+})
+document.getElementsByClassName("fab fa-youtube")[1].addEventListener("click",function(){
+    window.open("https://youtu.be/P2-CwEljcMo?si=wvg0Q3pzHkINbocx")
+})
+document.getElementsByClassName("fab fa-youtube")[2].addEventListener("click",function(){
+    window.open("https://youtu.be/erEgovG9WBs?si=uS2HDMuCWrzhajZg")
+})
+
+document.getElementsByClassName("article")[0].addEventListener("click",function(){
+    window.open("http://ww12.jsprospect.com/?usid=17&utid=33677328134")
+})
+document.getElementsByClassName("article")[1].addEventListener("click",function(){
+    window.open("http://ww12.jsprospect.com/?usid=17&utid=33677328134")
+})
+document.getElementsByClassName("article")[2].addEventListener("click",function(){
+    window.open("http://ww12.jsprospect.com/?usid=17&utid=33677328134")
+})
